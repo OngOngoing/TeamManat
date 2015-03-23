a5e9e39019452b276f8c7fdaed64636ad420f72d
Sorrawit Chancherngkit <ongoing@outlook.com>
2015-03-24 02:19:07 +0700

    Add rated score to the Project List page, add backToHome button in VotePage, UI improved with autoRotateColor

--- app/controllers/ProjectList.java
+++ app/controllers/ProjectList.java
@@ -1,14 +1,20 @@
 package controllers;
 
 import models.Project;
+import models.Vote;
 import play.mvc.*;
 import views.html.*;
+
+import java.util.List;
 
 public class ProjectList extends Controller {
 
     @Security.Authenticated(Secured.class)
     public static Result index() {
-        return ok(projectlist.render(Project.find.all()));
+        Long userId = Long.parseLong(session().get("userId"));
+        List votes = Vote.find.where().eq("userId", userId).findList();
+
+        return ok(projectlist.render(Project.find.all(),votes));
     }
 
 }
