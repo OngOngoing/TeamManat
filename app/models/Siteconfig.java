996045e1a6759e4e17ec7967c2f7bf4e1446339a
ChinCluBi <chincub@gmail.com>
2015-04-02 10:04:50 +0700

    Fix bug

--- app/models/Siteconfig.java
+++ app/models/Siteconfig.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import play.db.ebean.*;
 
 @Entity
-public class WebConfig extends Model{
+public class Siteconfig extends Model{
 
     public final static int TYPE_DATE = 1, TYPE_INTEGER = 2, TYPE_STRING = 3, TYPE_BOOLEAN = 4;
     @Id
@@ -14,19 +14,19 @@ public class WebConfig extends Model{
     public String keyValue;
     public int idType; // 1 = Date ; 2 = Integer ; 3 = String ; 4 = boolean
     public String description;
-    public WebConfig(String k, String v, int type, String d){
+    public Siteconfig(String k, String v, int type, String d){
         this.keyName = k;
         this.keyValue = v;
         this.idType = type;
         this.description = d;
     }
-    public static WebConfig create(String k, String v, int type, String description){
-        if(WebConfig.find.where().eq("keyName",k).findUnique()==null) {
-            WebConfig con = new WebConfig(k, v, type, description);
+    public static Siteconfig create(String k, String v, int type, String description){
+        if(Siteconfig.find.where().eq("keyName",k).findUnique()==null) {
+            Siteconfig con = new Siteconfig(k, v, type, description);
             con.save();
             return con;
         }
         return null;
     }
-    public static Finder<Long, WebConfig> find = new Finder<Long, WebConfig>(Long.class, WebConfig.class);
+    public static Finder<Long, Siteconfig> find = new Finder<Long, Siteconfig>(Long.class, Siteconfig.class);
 }
