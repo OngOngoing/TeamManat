af9ef8415ee38ffbe315797e8cbd5273c69115a5
ChinCluBi <chincub@gmail.com>
2015-04-02 09:52:50 +0700

    Edit name of model

--- app/models/webConfig.java
+++ app/models/webConfig.java
@@ -4,13 +4,13 @@ import javax.persistence.*;
 import play.db.ebean.*;
 
 @Entity
-public class webConfig extends Model{
+public class WebConfig extends Model{
     @Id
     public Long id;
 
     public String keyName;
     public String keyValue;
-    public webConfig(String k, String v){
+    public WebConfig(String k, String v){
         this.keyName = k;
         this.keyValue = v;
     }
