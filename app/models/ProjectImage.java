ed451d464b0dd653dcfc09c9c91bd6ae513b7f8f
ChinCluBi <chincub@gmail.com>
2015-04-14 22:31:13 +0700

    Screen Shot for project

--- app/models/ProjectImage.java
+++ app/models/ProjectImage.java
@@ -1,7 +1,29 @@
 package models;
 
-/**
- * Created by Chin on 4/14/2015.
- */
-public class ProjectImage {
+import javax.persistence.*;
+import play.data.validation.Constraints.Required;
+import play.db.ebean.*;
+import java.util.List;
+
+@Entity
+public class ProjectImage extends Model {
+    @Id
+    private Long Id;
+    @Required
+    private Long projectId;
+    @Lob
+    private byte[] image;
+    private static Finder<Long, ProjectImage> find = new Finder<Long, ProjectImage>(Long.class, ProjectImage.class);
+    private ProjectImage(Long projectId, byte[] image){
+        this.projectId = projectId;
+        this.image = image;
+    }
+    public ProjectImage create(Long projectId, byte[] image){
+        ProjectImage img = new ProjectImage(projectId, image);
+        img.save();
+        return img;
+    }
+    public List<ProjectImage> findAllPicByProjectId(Long projectId){
+        return find.where().eq("projectId", projectId).findList();
+    }
 }
