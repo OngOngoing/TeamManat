724bf7999ca456fecfe9e3d186801df292d269f6
ChinCluBi <chincub@gmail.com>
2015-04-20 01:46:01 +0700

    UI timmer for mobile

--- public/javascripts/projectlist.js
+++ public/javascripts/projectlist.js
@@ -12,6 +12,15 @@ $(function() {
         onEnd: function() {
             $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
         }
-});
+    });
+    $('#countdown-s').countdown({
+        date: $('#countdown-s').attr("end-date"),
+        render: function(data) {
+            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
+        },
+        onEnd: function() {
+            $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
+        }
+    });
 });
 
