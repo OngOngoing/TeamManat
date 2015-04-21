4361b08979c0ca73820518ac747aa55cc83c96f5
ChinCluBi <chincub@gmail.com>
2015-04-21 15:55:36 +0700

    Edit timer for Safari and Fix UI

--- app/controllers/ProjectList.java
+++ app/controllers/ProjectList.java
@@ -4,6 +4,9 @@ import models.*;
 import play.mvc.*;
 import views.html.*;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Map;
 
 public class ProjectList extends Controller {
@@ -14,8 +17,24 @@ public class ProjectList extends Controller {
         User user = User.findByUserId(userId);
         Settings setting = Settings.value("stopTime");
         Map<Long,Boolean> mappedRate = Rate.getRatedVoteAndProjectIdMappingByUserId(userId);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("M-d-y HH:mm");
+        Calendar calendar = Calendar.getInstance();
+        String date = setting.keyValue;
+        String _time = "";
+        try {
+            calendar.setTime(dateFormat.parse(date));
+            int year = calendar.get(Calendar.YEAR);
+            int month = calendar.get(Calendar.MONTH);
+            int day = calendar.get(Calendar.DAY_OF_MONTH);
+            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+            int minute = calendar.get(Calendar.MINUTE);
+            int second = calendar.get(Calendar.SECOND);
+            _time = String.format("%d/%02d/%02d %02d:%02d:%02d", year, month + 1, day, hour, minute, second);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         response().setHeader("Cache-Control","no-cache");
-        return ok(projectlist.render(Project.findAll(),mappedRate,user,setting));
+        return ok(projectlist.render(Project.findAll(),mappedRate,user,_time));
     }
 
 }
--- public/javascripts/projectlist.js
+++ public/javascripts/projectlist.js
@@ -1,26 +1,14 @@
 $(function() {
     var convert = new Markdown.getSanitizingConverter().makeHtml;
-    $('.projectDescription').each(function(){
+    $('.projectDescription').each(function () {
         $(this).html(convert($(this).attr("data")));
     });
-
-    $('#countdown').countdown({
-        date: $('#countdown').attr("end-date"),
-        render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-        },
-        onEnd: function() {
-            $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
-        }
-    });
-    $('#countdown-s').countdown({
-        date: $('#countdown-s').attr("end-date"),
-        render: function(data) {
-            $(this.el).html("<div>" + this.leadingZeros(data.days, 3) + " <span>days</span></div><div>" + this.leadingZeros(data.hours, 2) + " <span>hrs</span></div><div>" + this.leadingZeros(data.min, 2) + " <span>min</span></div><div>" + this.leadingZeros(data.sec, 2) + " <span>sec</span></div>");
-        },
-        onEnd: function() {
-            $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
-        }
+    $('#countdown').countdown($('#countdown').attr("end-date"), function (event) {
+        var $this = $(this).html(event.strftime("<div>"
+        + '%d<span>days</span></div><div> '
+        + '%H<span>hrs</span></div><div>'
+        + '%M<span>min</span></div><div> '
+        + '%S<span>sec</span></div><div>'));
     });
 });
 
