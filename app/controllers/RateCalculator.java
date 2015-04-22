c794e8616f3563992d2a0bd6cd539397e18e7a1b
Worapon Olanwanitchakul <smart2538@gmail.com>
2015-04-22 23:33:28 +0700

    Rating Result Add more page

--- app/controllers/RateCalculator.java
+++ app/controllers/RateCalculator.java
@@ -3,10 +3,14 @@ package controllers;
 import models.*;
 import play.mvc.*;
 import views.html.ratecalpage;
+import views.html.ratecalpagebycriteria;
 
 import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 
 public class RateCalculator extends Controller {
@@ -55,7 +59,7 @@ public class RateCalculator extends Controller {
         response().setHeader("Cache-Control","no-cache");
         return ok(ratecalpage.render(thisUser,projects,criteria,comments,result));
     }
-    /*public static Result rateSortByCriteria() {
+    public static Result rateSortByCriteria() {
         boolean isTimeUp = Settings.isTimeUp();
         Long userId = Long.parseLong(session().get("userId"));
         User thisUser = User.findByUserId(userId);
@@ -63,17 +67,18 @@ public class RateCalculator extends Controller {
             flash("error","Please wait until the rating session is closed. Sorry for the inconvenience.");
             return redirect(routes.ProjectList.index());
         }
-        List<RateCriterion> criteria = RateCriterion.findAll();
+        List< List< Map<String,String> > > result = new ArrayList < List< Map<String,String> > >(); 
+        List <RateCriterion> criteria = RateCriterion.findAll();
         List<Project> projects = Project.findAll();
-        Map<String, String> i = new HashedMap();
         for(RateCriterion c : criteria){
-            List<Double> score  = new ArrayList<Double>();
+            List<Map<String,String>> score  = new ArrayList<Map<String,String>>();
             for(Project p : projects){
+                Map<String, String> i = new HashMap<String,String>();
                 double sum = 0;
                 int count = 0;
-                Rate rates = Rate.findListByProjectIdAndCriteriaId(p.id,c.id);
+                List<Rate> rates = Rate.findListByProjectIdAndCriteriaId(p.id,c.id);
                 for(Rate r : rates){
-                    sum = += r.score;
+                    sum += r.score;
                     count++;
                 }
                 if(count == 0){
@@ -81,17 +86,29 @@ public class RateCalculator extends Controller {
                 }else{
                     sum = sum/count;
                 }
-                score.put(sum);
+                i.put("projectName", p.projectName);
+                i.put("score", String.format("%.2f",sum));
+                score.add(i);
             }
-            Collections.sort(score);
-            Collections.sort(score);
-            for(double s : score ){
-                i.put("Score" , String.format("%.2f",s));
-                i.put("Project", Project.findByProjectIdAndProjectIdAndCriteriaId)
-            }
+
+            /*for(int i = 0 ; i<score.size() ; i++){
+                for(int j = i ;j<score.size(). j++){
+                    if(Double.parseDouble(score.get(i).get("score")) < Double.parseDouble(score.get(j).get("score")) ){
+                        score.get(i) = score.get()
+                    }
+                }
+            }*/
+
+            Collections.sort(score, Collections.reverseOrder(new Comparator() {
+                public int compare(Object o1, Object o2) {
+                    Double sum1 = (Double.parseDouble(((Map<String,String>)o1).get("score")));
+                    Double sum2 = (Double.parseDouble(((Map<String,String>)o2).get("score")));
+                    return sum1.compareTo(sum2);
+                }
+            }));
+            result.add(score);
         }
-        
         response().setHeader("Cache-Control","no-cache");
-        return ok(ratecalpage.render(criteria,result));
-    }*/
+        return ok(ratecalpagebycriteria.render(thisUser,criteria,result));
+    }
 }
