68dd2ddc838a2d339109a4436a5bae30b5dc3b2f
ChinCluBi <chincub@gmail.com>
2015-04-23 04:32:59 +0700

    Fix timer for mobile

--- public/javascripts/projectlist.js
+++ public/javascripts/projectlist.js
@@ -3,16 +3,18 @@ $(function () {
     $('.projectDescription').each(function () {
         $(this).html(convert($(this).attr("data")));
     });
-    $('#countdown').countdown($('#countdown').attr("end-date"))
-        .on('update.countdown', function (event) {
-            var $this = $(this).html(event.strftime("<div>"
-            + '%d<span>days</span></div><div> '
-            + '%H<span>hrs</span></div><div>'
-            + '%M<span>min</span></div><div> '
-            + '%S<span>sec</span></div><div>'));
-        }).on('finish.countdown', function (event) {
-            $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
-        });
+    $('#countdown').each(function(){
+      $(this).countdown($('#countdown').attr("end-date"))
+          .on('update.countdown', function (event) {
+              var $this = $(this).html(event.strftime("<div>"
+              + '%d<span>days</span></div><div> '
+              + '%H<span>hrs</span></div><div>'
+              + '%M<span>min</span></div><div> '
+              + '%S<span>sec</span></div><div>'));
+          }).on('finish.countdown', function (event) {
+              $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
+          });
+    });
     $('#countdown-s').countdown($('#countdown-s').attr("end-date"))
         .on('update.countdown', function (event) {
             var $this = $(this).html(event.strftime("<div>"
@@ -21,7 +23,8 @@ $(function () {
             + '%M<span>min</span></div><div> '
             + '%S<span>sec</span></div><div>'));
         }).on('finish.countdown', function (event) {
-            $('#count-down').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
+            console.log("time up");
+            $('#count-down-s').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
         });
 });
 
