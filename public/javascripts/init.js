fafef3c73ff10ed0bff0253ca46ce81a135cbcf8
ChinCluBi <chincub@gmail.com>
2015-05-05 17:23:21 +0700

    Add inbox to all page

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -1,3 +1,46 @@
 $(document).ready(function(){
     $(".button-collapse").sideNav();
+    $('.dropdown-button').dropdown({
+        inDuration: 0,
+        belowOrigin: true
+    });
+    $('#inbox').click(function(){
+        $('.pointer').css('left', $(this).position().left - $('#inbox_message').position().left + 15 );
+    });
+    $('#scrollbar-message').slimscroll({
+        color: '#000',
+        size: '10px',
+        width: '100%',
+        height: 'auto',
+        maxHeight: '250px'
+    });
+    $('li[id^="inbox-id-"]').click(function(){
+        var that = $(this);
+        $.ajax({
+            type: "get",
+            url: "../inbox/" + $(this).attr("inbox-id"),
+            dataType: "text",
+            success: function (msg) {
+                $("#icon-inbox-" + that.attr("inbox-id")).removeClass();
+                if(msg == "READ") {
+                    if($('#i-inbox').attr('data-badge') == 1){
+                        $('#i-inbox').removeAttr('data-badge');
+                    }else{
+                        $('#i-inbox').attr('data-badge', $('#i-inbox').attr('data-badge') - 1);
+                    }
+                    $("#icon-inbox-" + that.attr("inbox-id")).attr('class', 'mdi-content-drafts left');
+                    that.attr('data-tooltip', 'Make as unread');
+                }else{
+                    if($('#i-inbox').attr('data-badge') == undefined){
+                        $('#i-inbox').attr('data-badge', 1);
+                    }else{
+                        $('#i-inbox').attr('data-badge', $('#inbox').attr('data-badge') + 1);
+                    }
+                    $("#icon-inbox-" + that.attr("inbox-id")).attr('class', 'mdi-content-markunread left');
+                    that.attr('data-tooltip', 'Make as read');
+                }
+            }
+        });
+    });
+    $('.tooltipped').tooltip({delay: 50});
   });
