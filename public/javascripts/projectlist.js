e4cf5db6568c25ff29b3ea540a939815f2f11641
ChinCluBi <chincub@gmail.com>
2015-05-05 10:30:08 +0700

    Modify inbox function and make inbox in rateproject page

--- public/javascripts/projectlist.js
+++ public/javascripts/projectlist.js
@@ -10,7 +10,6 @@ $(function () {
     });
     $('#inbox').click(function(){
         $('.pointer').css('left', $(this).position().left - $('#inbox_message').position().left + 15 );
-        $('.badge').removeAttr('data-badge');
     });
     $('#scrollbar-message').slimscroll({
         color: '#000',
@@ -50,9 +49,19 @@ $(function () {
             success: function (msg) {
                 $("#icon-inbox-" + that.attr("inbox-id")).removeClass();
                 if(msg == "READ") {
+                    if($('#i-inbox').attr('data-badge') == 1){
+                        $('#i-inbox').removeAttr('data-badge');
+                    }else{
+                        $('#i-inbox').attr('data-badge', $('#i-inbox').attr('data-badge') - 1);
+                    }
                     $("#icon-inbox-" + that.attr("inbox-id")).attr('class', 'mdi-content-drafts left');
                     that.attr('data-tooltip', 'Make as unread');
                 }else{
+                    if($('#i-inbox').attr('data-badge') == undefined){
+                        $('#i-inbox').attr('data-badge', 1);
+                    }else{
+                        $('#i-inbox').attr('data-badge', $('#inbox').attr('data-badge') + 1);
+                    }
                     $("#icon-inbox-" + that.attr("inbox-id")).attr('class', 'mdi-content-markunread left');
                     that.attr('data-tooltip', 'Make as read');
                 }
