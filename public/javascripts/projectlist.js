3dea34abbba6df4bd9fc7a45f1155bb55dd98a9e
OngOngoing <ongoing@outlook.com>
2015-05-07 01:27:35 +0700

    Revamp Achievement's progress bar

--- public/javascripts/projectlist.js
+++ public/javascripts/projectlist.js
@@ -30,29 +30,34 @@ $(function () {
         }).on('finish.countdown', function (event) {
             $('#count-down-s').html('<div class="count-down-label col s12 red-text center">Time\'s up<i class="mdi-device-access-alarm"></i></div>');
         });
-    var prevScroll = 0,
-        curDir = 'down',
-        prevDir = 'up';
+});
 
-    $(window).scroll(function(){
-        if ($(this).scrollTop() >= prevScroll) {
-            if($(this).scrollTop() >= 45 && $('#mobile-demo').position().left != 0) {
-                curDir = 'down';
-                if (curDir != prevDir) {
-                    $('#head').addClass('scroll');
-                    $('#head-2').addClass('scroll');
-                    prevDir = curDir;
-                }
-            }
-        } else {
-            curDir = 'up';
-            if (curDir != prevDir) {
-                $('#head').removeClass('scroll');
-                $('#head-2').removeClass('scroll');
-                prevDir = curDir;
-            }
+
+
+var startColor = '#FC5B3F';
+var endColor = '#6FD57F';
+
+window.onload = function onLoad() {
+    var circle = new ProgressBar.Circle('#progress-circle', {
+        strokeWidth: 3,
+        trailWidth: 1,
+        text: {
+            value: '0'
+        },
+        color: startColor,
+        duration: 1500,
+        easing: 'easeInOut',
+        step: function(state, bar) {
+            bar.setText((bar.value() * 100).toFixed(0) + '%');
+            $('.progressbar-text').css('color',state.color);
+            bar.path.setAttribute('stroke',state.color);
+            bar.path.setAttribute('stroke-width', state.width);
         }
-        prevScroll = $(this).scrollTop();
+
     });
 
-});
+    circle.animate($('#progress-circle').attr('value'), {
+        from: {color: startColor, width: 0.5},
+        to: {color: endColor, width: 3}
+    });
+};
