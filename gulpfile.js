8bfd03be16f317c7538766fcb667477ef7283202
ChinCluBi <chincub@gmail.com>
2015-08-02 21:26:57 +0700

    Init gulp to compile scss

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp')
+var sass = require('gulp-ruby-sass')
+var concat = require('gulp-concat')
+var minifyCSS = require('gulp-minify-css')
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
+var replace = require('gulp-replace')
+
+var scss_file = 'style.scss'
+
+gulp.task('scss', function () {
+    return sass(scss_file, { style: 'nested' })
+        .pipe(replace('/*!', '/*'))
+        .pipe(minifyCSS())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('public/stylesheets'))
+})
+
+gulp.task('js', function () {
+    return gulp.src(['assets/js/*.js'])
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('assets/javascript'))
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest('assets/javascript'))
+})
+
+gulp.task('watch', function () {
+    gulp.watch(scss_file, ['scss'])
+    gulp.watch('assets/js/*.js', ['js'])
+})
+
+gulp.task('watch-withoutjs', function () {
+    gulp.watch(scss_file, ['scss'])
+})
+
+gulp.task('default', ['watch-withoutjs'])
